092f637b5cde5fd704cf1d0d42dda159f2c2ef7c
jon_y <jon_y@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-09-04 09:24:06 +0000

    Make unicode file open feature report 1 when WANT_WIN32_UNICODE is defined.
    git-svn-id: svn://scm.orgis.org/mpg123/branches/mpg123-unicode-new@2315 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/feature.c
+++ src/libmpg123/feature.c
@@ -5,7 +5,12 @@ int mpg123_feature(const enum mpg123_feature_set key)
 {
 	switch(key)
 	{
-		case mpg123_feature_utf8open: return WANT_WIN32_UNICODE+0; /* Do we need to make sure its 1 or 0? */
+		case mpg123_feature_utf8open:
+		#ifdef WANT_WIN32_UNICODE
+		  return 1;
+		#else
+		  return 0;
+		#endif /* WANT_WIN32_UNICODE */
 		default: return 0;
 	}
 }
