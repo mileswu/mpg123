eec0244a3b62ee6598c8a68b85a42bc91ac4423e
jon_y <jon_y@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-09-04 09:02:34 +0000

    Remove non-working 64bit offset feature report
    git-svn-id: svn://scm.orgis.org/mpg123/branches/mpg123-unicode-new@2313 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/feature.c
+++ src/libmpg123/feature.c
@@ -5,8 +5,7 @@ int mpg123_feature(const enum mpg123_feature_set key)
 {
 	switch(key)
 	{
-		case mpg123_feature_utf8open: return WANT_WIN32_UNICODE+0; break; /* Do we need to make sure its 1 or 0? */
-		case mpg123_feature_64bit_files: return !(!(_FILE_OFFSET_BITS+0)); break;
+		case mpg123_feature_utf8open: return WANT_WIN32_UNICODE+0; /* Do we need to make sure its 1 or 0? */
 		default: return 0;
 	}
 }
