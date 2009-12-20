a67dd15be28d423e978be0b78dbd84a9925cedc9
jon_y <jon_y@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-12-20 01:39:27 +0000

    Move more code to win32_support_net.c
    git-svn-id: svn://scm.orgis.org/mpg123/branches/win32_networking@2504 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/win32_support_net.c
+++ src/win32_support_net.c
@@ -6,7 +6,283 @@
 #include "resolver.h"
 #include <errno.h>
 
-#ifdef NETWORK
+#if defined (WANT_WIN32_SOCKETS)
+#ifdef DEBUG
+#define msgme(x) win32_net_msg(x,__FILE__,__LINE__)
+#define msgme1 win32_net_msg(1,__FILE__,__LINE__)
+#define msgme_sock_err(x) if ((x)==SOCKET_ERROR) {msgme1;}
+#else
+#define msgme(x) x
+#define msgme1 do{} while(0)
+#define msgme_sock_err(x) x
+#endif
+struct ws_local
+{
+  int inited;
+  SOCKET local_socket; /*stores last connet in win32_net_open_connection*/
+  WSADATA wsadata;
+};
+
+static struct ws_local ws;
+#ifdef DEBUG
+static void win32_net_msg (const int err, const char * const filedata, const int linedata)
+{
+  char *errbuff;
+  int lc_err;
+  if (err)
+  {
+    lc_err = WSAGetLastError();
+    FormatMessage(
+      FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+      NULL,
+      lc_err,
+      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      (LPTSTR) &errbuff,
+      0,
+      NULL );
+    fprintf(stderr, "[%s:%d] [WSA2: %d] %s", filedata, linedata, lc_err, errbuff);
+    LocalFree (errbuff);
+  }
+}
+#endif
+
+void win32_net_init (void)
+{
+  ws.inited = 1;
+  switch ((WSAStartup(MAKEWORD(2,2), &ws.wsadata)))
+  {
+    case WSASYSNOTREADY: debug("WSAStartup failed with WSASYSNOTREADY"); break;
+    case WSAVERNOTSUPPORTED: debug("WSAStartup failed with WSAVERNOTSUPPORTED"); break;
+    case WSAEINPROGRESS: debug("WSAStartup failed with WSAEINPROGRESS"); break;
+    case WSAEPROCLIM: debug("WSAStartup failed with WSAEPROCLIM"); break;
+    case WSAEFAULT: debug("WSAStartup failed with WSAEFAULT"); break;
+    default:
+    break;
+  }
+}
+
+void win32_net_deinit (void)
+{
+  debug("Begin winsock cleanup");
+  if (ws.inited)
+  {
+    if (ws.inited >= 2)
+    {
+      debug1("ws.local_socket = %d", ws.local_socket);
+      msgme_sock_err(shutdown(ws.local_socket, SD_BOTH));
+      msgme_sock_err(closesocket(ws.local_socket));
+    }
+    WSACleanup();
+    ws.inited = 0;
+  }
+}
+
+static void win32_net_nonblock(SOCKET sock)
+{
+  u_long mode = 1;
+  msgme_sock_err(ioctlsocket(sock, FIONBIO, &mode));
+}
+
+static void win32_net_block(SOCKET sock)
+{
+  u_long mode = 0;
+  msgme_sock_err(ioctlsocket(sock, FIONBIO, &mode));
+}
+
+ssize_t win32_net_read (int fildes, void *buf, size_t nbyte)
+{
+  debug1("Attempting to read %d bytes from network.", nbyte);
+  ssize_t ret;
+  msgme_sock_err(ret = (ssize_t) recv(ws.local_socket, buf, nbyte, 0));
+  debug1("Read %d bytes from network.", ret);
+
+  return ret;
+}
+
+static int get_sock_ch (SOCKET sock)
+{
+  char c;
+  int ret;
+  msgme_sock_err(ret = recv (sock, &c, 1, 0));
+  if (ret == 1)
+    return (((int) c)&0xff);
+  return -1;
+}
+/* Addapted from from newlib*/
+char *win32_net_fgets(char *s, int n, SOCKET stream)
+{
+  char c = 0;
+  char *buf;
+  buf = s;
+  debug1("Pseudo net fgets attempts to read %d bytes from network.", n - 1);
+  while (--n > 0 && (c = get_sock_ch (stream)) != -1)
+  {
+    *s++ = c;
+    if (c == '\n' || c == '\r')
+      break;
+  }
+  debug1("Pseudo net fgets got %u bytes.", (s - buf));
+  if (c == -1 && s == buf)
+  {
+    debug("Pseudo net fgets met a premature end.");
+    return NULL;
+  }
+  *s = 0;
+  return buf;
+}
+
+ssize_t win32_net_write (int fildes, const void *buf, size_t nbyte)
+{
+  debug1("Attempting to write %d bytes to network.", nbyte);
+  ssize_t ret;
+  msgme_sock_err((ret = (ssize_t) send(ws.local_socket, buf, nbyte, 0)));
+  debug1("wrote %d bytes to network.", ret);
+
+  return ret;
+}
+
+off_t win32_net_lseek (int a, off_t b, int c)
+{
+  debug("lseek on a socket called!");
+  return -1;
+}
+
+void win32_net_replace (mpg123_handle *fr)
+{
+  debug("win32_net_replace ran");
+  mpg123_replace_reader(fr, win32_net_read, win32_net_lseek);
+}
+
+static int win32_net_timeout_connect(SOCKET sockfd, const struct sockaddr *serv_addr, socklen_t addrlen)
+{
+	debug("win32_net_timeout_connect ran");
+	if(param.timeout > 0)
+	{
+		int err;
+		win32_net_nonblock(sockfd);
+		err = connect(sockfd, serv_addr, addrlen);
+		if(err != SOCKET_ERROR)
+		{
+			debug("immediately successful");
+			win32_net_block(sockfd);
+			return 0;
+		}
+		else if(WSAGetLastError() == WSAEWOULDBLOCK) /*WSAEINPROGRESS would not work here for some reason*/
+		{
+			struct timeval tv;
+			fd_set fds;
+			tv.tv_sec = param.timeout;
+			tv.tv_usec = 0;
+
+			debug("in progress, waiting...");
+
+			FD_ZERO(&fds);
+			FD_SET(sockfd, &fds);
+			err = select(sockfd+1, NULL, &fds, NULL, &tv);
+			if(err != SOCKET_ERROR)
+			{
+				socklen_t len = sizeof(err);
+				if((getsockopt(sockfd, SOL_SOCKET, SO_ERROR, (char *)&err, &len) != SOCKET_ERROR)
+				   && (err == 0) )
+				{
+					debug("non-blocking connect has been successful");
+					win32_net_block(sockfd);
+					return 0;
+				}
+				else
+				{
+					error1("connection error: %s", msgme(err));
+					return -1;
+				}
+			}
+			else if(err == 0)
+			{
+				error("connection timed out");
+				return -1;
+			}
+			else
+			{
+				/*error1("error from select(): %s", strerror(errno));*/
+				debug("error from select():");
+				msgme1;
+				return -1;
+			}
+		}
+		else
+		{
+			/*error1("connection failed: %s", strerror(errno));*/
+			debug("connection failed: ");
+			msgme1;
+			return err;
+		}
+	}
+	else
+	{
+		if(connect(sockfd, serv_addr, addrlen) == SOCKET_ERROR)
+		{
+			/*error1("connecton failed: %s", strerror(errno));*/
+			debug("connecton failed");
+			msgme1;
+			return -1;
+		}
+		else {
+		debug("win32_net_timeout_connect succeed");
+		return 0; /* _good_ */
+		}
+	}
+}
+
+SOCKET win32_net_open_connection(mpg123_string *host, mpg123_string *port)
+{
+	struct addrinfo hints;
+	struct addrinfo *addr, *addrlist;
+	SOCKET addrcount, sock = SOCKET_ERROR;
+
+	if(param.verbose>1) fprintf(stderr, "Note: Attempting new-style connection to %s\n", host->p);
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family   = AF_UNSPEC; /* We accept both IPv4 and IPv6 ... and perhaps IPv8;-) */
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	debug2("Atempt resolve/connect to %s:%s", host->p, port->p);
+	msgme(addrcount = getaddrinfo(host->p, port->p, &hints, &addrlist));
+
+	if(addrcount <0)
+	{
+		error3("Resolving %s:%s: %s", host->p, port->p, gai_strerror(addrcount));
+		return -1;
+	}
+
+	addr = addrlist;
+	while(addr != NULL)
+	{
+		sock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+		if (sock == INVALID_SOCKET)
+		{
+			msgme1;
+		}
+		else
+		{
+			if(win32_net_timeout_connect(sock, addr->ai_addr, addr->ai_addrlen) == 0)
+			break;
+			debug("win32_net_timeout_connect error, closing socket");
+			msgme(closesocket(sock));
+			sock=SOCKET_ERROR;
+		}
+		addr=addr->ai_next;
+	}
+	if(sock == SOCKET_ERROR) {error2("Cannot resolve/connect to %s:%s!", host->p, port->p);}
+	else
+	{
+	  debug1("Saving socket %d",sock);
+	  ws.local_socket = sock;
+	  ws.inited = 2;
+	}
+
+	freeaddrinfo(addrlist);
+	return sock;
+}
+
 static size_t win32_net_readstring (mpg123_string *string, size_t maxlen, FILE *f)
 {
 	debug2("Attempting readstring on %d for %d bytes", f ? fileno(f) : 0, maxlen);
@@ -312,4 +588,4 @@ int win32_net_http_open(char* url, struct httpdata *hd)
 {
   return -1;
 }
-#endif
+#endif /*WANT_WIN32_SOCKETS */
